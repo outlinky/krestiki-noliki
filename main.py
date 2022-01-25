40c21bf41248110020ca5e973c776160e82376dc
outlinky <j4red@ya.ru>
2022-01-26 00:12:52 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+board = list(range(1, 10))
+win_lines = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)]
+
+
+def draw_board():
+    print('-------------')
+    for i in range(3):
+        print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|')
+    print('-------------')
+
+
+def player_move(player_token):
+    while True:
+        value = input(f'Куда поставить {player_token} ? ')
+        if not (value in '123456789'):
+            print('Ошибочный ввод! Повторите!')
+            continue
+        value = int(value)
+        if str(board[value - 1]) in 'XO':
+            print('Эта клетка уже занята!')
+            continue
+        board[value - 1] = player_token
+        break
+
+
+def check_win():
+    for each in win_lines:
+        if (board[each[0] - 1]) == (board[each[1] - 1]) == (board[each[2] - 1]):
+            return board[each[1] - 1]
+    else:
+        return False
+
+
+def main():
+    counter = 0
+    while True:
+        draw_board()
+        if counter % 2 == 0:
+            player_move('X')
+        else:
+            player_move('O')
+        if counter > 3:
+            winner = check_win()
+            if winner:
+                draw_board()
+                print(winner, 'выиграл!')
+                break
+        counter += 1
+        if counter > 8:
+            draw_board()
+            print('Победила дружба!')
+            break
+
+
+main()
